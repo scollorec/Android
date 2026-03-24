57975ef75f0f3e01c08cedb3d3afd11d72a0b274
scollorec <132073589+scollorec@users.noreply.github.com>
2026-03-24 14:56:52 +0000

    Quick update

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
-const cacheName = 'v1-cache';
-const assets = ['./index.html', './style.css', './script.js'];
+const CACHE_NAME = 'feed-v1';
 
-// Install event: Caches the files
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open(cacheName).then(cache => cache.addAll(assets))
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json'
+            ]);
+        })
     );
 });
 
-// Fetch event: Serves files from cache if offline
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
     );
 });
